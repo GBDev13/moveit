dad3608234a0dc01fd46cc402355703771ae5e59
Gabriel Borges <gbdesigns13@gmail.com>
2021-02-28 23:19:45 -0300

    Twitter

--- src/pages/api/twitter.tsx
+++ src/pages/api/twitter.tsx
@@ -9,7 +9,7 @@ export default async function(request: NextApiRequest, response: NextApiResponse
   const {level, challenges, experience} = request.query;
 
     const dir = path.resolve('./public')
-    const img = fs.readFileSync(dir).toString('utf-8')
+    // const img = fs.readFileSync(dir).toString('utf-8')
     // const handle = handlebars.compile(img)
     // const html = handle({ level, challenges, experience })
     // const image = await nodeHtmlToImage({
@@ -17,5 +17,5 @@ export default async function(request: NextApiRequest, response: NextApiResponse
     // })
     // response.writeHead(200, { 'Content-Type': 'image/png' })
     // response.end(image, 'binary')
-    response.status(200).json({dir, img})
+    response.status(200).json({dir})
 }
