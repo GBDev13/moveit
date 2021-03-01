2d530470e7fea95f9b65db6eae3d7b2d16de9704
Gabriel Borges <gbdesigns13@gmail.com>
2021-02-28 23:35:17 -0300

    Twitter

--- src/pages/api/twitter.tsx
+++ src/pages/api/twitter.tsx
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import path from 'path';
-import fs from 'fs';
+import path, { join } from 'path';
+import fs, { readFileSync } from 'fs';
 import handlebars  from 'handlebars';
 import nodeHtmlToImage from "node-html-to-image";
 
@@ -8,8 +8,9 @@ export default async function(request: NextApiRequest, response: NextApiResponse
 
   const {level, challenges, experience} = request.query;
 
-    const dir = path.resolve('./public', 'twitter.hbs')
-    const img = fs.readFileSync(dir).toString('utf-8')
+    // const dir = path.resolve('./public', 'twitter.hbs')
+    // const img = fs.readFileSync(dir).toString('utf-8')
+    const img = readFileSync(join(__dirname, 'public', 'twitter.hbs'), 'utf8')
     const handle = handlebars.compile(img)
     const html = handle({ level, challenges, experience })
     const image = await nodeHtmlToImage({
