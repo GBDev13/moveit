121f154016c2c0ccf62d855e65ff5a07b94df4f7
Gabriel Borges <gbdesigns13@gmail.com>
2021-02-28 23:22:55 -0300

    Twitter

--- src/pages/api/twitter.tsx
+++ src/pages/api/twitter.tsx
@@ -8,14 +8,14 @@ export default async function(request: NextApiRequest, response: NextApiResponse
 
   const {level, challenges, experience} = request.query;
 
-    const dir = path.resolve('./public')
-    // const img = fs.readFileSync(dir).toString('utf-8')
-    // const handle = handlebars.compile(img)
-    // const html = handle({ level, challenges, experience })
-    // const image = await nodeHtmlToImage({
-    //     html
-    // })
-    // response.writeHead(200, { 'Content-Type': 'image/png' })
-    // response.end(image, 'binary')
-    response.status(200).json({dir})
+    const dir = path.resolve('./public', 'twitter.hbs')
+    const img = fs.readFileSync(dir).toString('utf-8')
+    const handle = handlebars.compile(img)
+    const html = handle({ level, challenges, experience })
+    const image = await nodeHtmlToImage({
+        html
+    })
+    response.writeHead(200, { 'Content-Type': 'image/png' })
+    response.end(image, 'binary')
+    // response.status(200).json({dir})
 }
