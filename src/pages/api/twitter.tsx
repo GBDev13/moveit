b07b2ce49bf5cbe2beac2286dca93e55825f2dde
Gabriel Borges <gbdesigns13@gmail.com>
2021-02-28 22:53:32 -0300

    Twitter share test

--- src/pages/api/twitter.tsx
+++ src/pages/api/twitter.tsx
@@ -3,14 +3,12 @@ import path from 'path';
 import fs from 'fs';
 import handlebars  from 'handlebars';
 import nodeHtmlToImage from "node-html-to-image";
-import getConfig from 'next/config';
 
 export default async function(request: NextApiRequest, response: NextApiResponse) {
 
   const {level, challenges, experience} = request.query;
 
-    const { serverRuntimeConfig } = getConfig()
-    const dir = path.join(serverRuntimeConfig.PROJECT_ROOT,'./public/assets/twitter.hbs')
+    const dir = path.join(__dirname,'./public/assets/twitter.hbs')
     const img = fs.readFileSync(dir).toString('utf-8')
     const handle = handlebars.compile(img)
     const html = handle({ level, challenges, experience })
