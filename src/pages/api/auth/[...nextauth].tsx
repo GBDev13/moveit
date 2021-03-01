582e04fc8db3b49b66f2c1674439a1a246eb2a02
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-01 19:56:32 -0300

    Trying to get Github user infos

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -14,6 +14,18 @@ export default NextAuth({
     session: async (session, user) => {
       session.userId = user.id;    
       return Promise.resolve(session);
+    },
+    signIn: async (_user, _account, profile) => {
+      userData = {
+        username: profile.login,
+        githubProfile: `https://github.com/${profile.login}`,
+        level: 1,
+        totalExperience:0,
+        challengesCompleted:0,
+        currentExperience:0,
+      };
+
+      return Promise.resolve(true);
     }
   },
   
