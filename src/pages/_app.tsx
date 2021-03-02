d1fa52223ad47f7e0c7a6486dfaf5865e87a2404
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-01 21:04:08 -0300

    Improved SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,33 @@ function MyApp({ Component, pageProps }) {
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+
+        <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+        <meta name="MobileOptimized" content="320" />
+        <meta name="HandheldFriendly" content="True" />
+        <meta name="theme-color" content="#5965E0" />
+        <meta name="msapplication-TileColor" content="#5965E0" />
+        <meta name="google" content="notranslate" />
+
+        <meta property="og:title" content="Move.it - O pomodoro Gamificado!" />
+        <meta property="og:description" content="Faça suas tarefas serem 10x mais produtivas usando a técnica do pomodoro da forma mais divertida e saudável possível!" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Move.it - O pomodoro Gamificado!" />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/assets/ogimage.png`} />
+        <meta property="og:image:secure_url" content={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/assets/ogimage.png`} />
+        <meta property="og:image:alt" content="Thumbnail" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:title" content="Move.it - O pomodoro Gamificado!" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/assets/ogimage.png`} />
+        <meta name="twitter:image:src" content={`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/assets/ogimage.png`} />
+        <meta name="twitter:image:alt" content="Thumbnail" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="620" />
       </Head>
       <GlobalStyle />
         <Provider session={pageProps.session}>
