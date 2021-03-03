b7bbefe58c24067d3b8d8714095c9d5a035c3770
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-03 11:49:55 -0300

    PWA removed temporarily

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const withPWA = require("next-pwa");
-
 module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
 };
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-  },
-});
