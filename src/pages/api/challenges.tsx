c9b93156c4938d0e5fd3f1b4542809b11fcd3d64
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-07 07:47:53 -0300

    Twitter share implementation

--- src/pages/api/challenges.tsx
+++ src/pages/api/challenges.tsx
@@ -1,107 +1,107 @@
-import { NowRequest, NowResponse} from '@vercel/node'
-import {MongoClient, Db, ObjectID} from 'mongodb';
-import url from 'url';
+// import { NowRequest, NowResponse} from '@vercel/node'
+// import {MongoClient, Db, ObjectID} from 'mongodb';
+// import url from 'url';
 
-let cachedDb:Db = null;
+// let cachedDb:Db = null;
 
-async function connectToDatabase(uri: string) {
-  if(cachedDb) {
-    return cachedDb;
-  }
+// async function connectToDatabase(uri: string) {
+//   if(cachedDb) {
+//     return cachedDb;
+//   }
 
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser:true
-  });
+//   const client = await MongoClient.connect(uri, {
+//     useNewUrlParser:true
+//   });
 
-  const dbName = url.parse(uri).pathname.substr(1);
+//   const dbName = url.parse(uri).pathname.substr(1);
 
-  const db = client.db(dbName)
+//   const db = client.db(dbName)
 
-  cachedDb = db;
+//   cachedDb = db;
 
-  return db;
-}
+//   return db;
+// }
 
-export default async (request:NowRequest, response:NowResponse) => {
-  const { id, level, challengesCompleted, currentExperience, totalExperience } = request.body;
+// export default async (request:NowRequest, response:NowResponse) => {
+//   const { id, level, challengesCompleted, currentExperience, totalExperience } = request.body;
 
-  const db = await connectToDatabase(process.env.NEXT_PUBLIC_DATABASE_URL);
+//   const db = await connectToDatabase(process.env.NEXT_PUBLIC_DATABASE_URL);
 
-  const collection = db.collection('users');
+//   const collection = db.collection('users');
 
-  if (request.method === "POST") {
-    var myquery = { "_id": ObjectID(id) };
-    if(level) {
-      const newValues = {$set : {"level": level}};
+//   if (request.method === "POST") {
+//     var myquery = { "_id": ObjectID(id) };
+//     if(level) {
+//       const newValues = {$set : {"level": level}};
   
-      const user = await collection.updateOne(myquery, newValues)
+//       const user = await collection.updateOne(myquery, newValues)
   
-      return response.status(200).json(user);
-    }
-    else if(challengesCompleted) {
-      const newValues = {$set : {"challengesCompleted": challengesCompleted}};
+//       return response.status(200).json(user);
+//     }
+//     else if(challengesCompleted) {
+//       const newValues = {$set : {"challengesCompleted": challengesCompleted}};
   
-      const user = await collection.updateOne(myquery, newValues)
+//       const user = await collection.updateOne(myquery, newValues)
   
-      return response.status(200).json(user);
-    }
-    else if(currentExperience) {
-      const newValues = {$set : {"currentExperience": currentExperience}};
+//       return response.status(200).json(user);
+//     }
+//     else if(currentExperience) {
+//       const newValues = {$set : {"currentExperience": currentExperience}};
   
-      const user = await collection.updateOne(myquery, newValues)
+//       const user = await collection.updateOne(myquery, newValues)
   
-      return response.status(200).json(user);
-    }
-    else if(totalExperience) {
-      const newValues = {$set : {"totalExperience": totalExperience}};
+//       return response.status(200).json(user);
+//     }
+//     else if(totalExperience) {
+//       const newValues = {$set : {"totalExperience": totalExperience}};
   
-      const user = await collection.updateOne(myquery, newValues)
+//       const user = await collection.updateOne(myquery, newValues)
   
-      return response.status(200).json(user);
-    }
-    else {
-      return response.status(400).json({message:'Error'})
-    }
-  }
+//       return response.status(200).json(user);
+//     }
+//     else {
+//       return response.status(400).json({message:'Error'})
+//     }
+//   }
   
-  else if(request.method === "GET") {
-    const { id, all } = request.query;
+//   else if(request.method === "GET") {
+//     const { id, all } = request.query;
 
-    if(id){
-      var myquery = { "_id": ObjectID(id) };
+//     if(id){
+//       var myquery = { "_id": ObjectID(id) };
 
-      const user = await collection.findOne(myquery);
+//       const user = await collection.findOne(myquery);
 
-      return response.status(200).json(user);
-    } else if (all){
+//       return response.status(200).json(user);
+//     } else if (all){
 
-      await collection.find({}).toArray((err, result) => {
-        if(err) {
-          return response.status(400).json({error: err})
-        }
-        return response.status(200).json(result);
-      });
+//       await collection.find({}).toArray((err, result) => {
+//         if(err) {
+//           return response.status(400).json({error: err})
+//         }
+//         return response.status(200).json(result);
+//       });
 
-    } else {
-      return response.status(400).json({message:'Error'})
-    }
-  }
+//     } else {
+//       return response.status(400).json({message:'Error'})
+//     }
+//   }
 
-  else if(request.method === "PUT") {
-    const { id, badge_id, description, image } = request.body;
+//   else if(request.method === "PUT") {
+//     const { id, badge_id, description, image } = request.body;
 
-    if(id){
-      var myquery = { "_id": ObjectID(id) };
+//     if(id){
+//       var myquery = { "_id": ObjectID(id) };
 
-      const user = await collection.updateOne(myquery, {$push: {achievements: {
-        badge_id,
-        description,
-        image: image
-      }}})
+//       const user = await collection.updateOne(myquery, {$push: {achievements: {
+//         badge_id,
+//         description,
+//         image: image
+//       }}})
 
-      return response.status(200).json(user);
-    } else {
-      return response.status(400).json({message:'Error'})
-    }
-  }
-}
+//       return response.status(200).json(user);
+//     } else {
+//       return response.status(400).json({message:'Error'})
+//     }
+//   }
+// }
