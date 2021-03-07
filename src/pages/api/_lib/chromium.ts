0d9dacafbf8895621066729b060b1967459cc9c2
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-07 06:47:48 -0300

    Changed packages versions

--- src/pages/api/_lib/chromium.ts
+++ src/pages/api/_lib/chromium.ts
@@ -9,7 +9,7 @@ async function getPage(isDev: boolean): Promise<Page> {
   }
 
   const options = await getOptions(isDev)
-  const browser = await puppeteer.launch(options);
+  const browser = await puppeteer.launch({...options, args:['--no-sandbox']});
 
   _page = await browser.newPage()
 
