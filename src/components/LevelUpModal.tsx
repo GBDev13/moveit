cc2f72a69471a645a6691d3f8b68573fcd095232
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-06 21:23:21 -0300

    New Twitter share method route

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -22,7 +22,7 @@ export function LevelUpModal() {
 
         <footer>
           <button type="button">
-            <a href={`https://twitter.com/intent/tweet?text=${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/twitter/?level=${level}&challenges=${challengesCompleted}&experience=${totalExperience}`}>Compartilhar no Twitter <Twitter /></a>
+            <a href={`https://twitter.com/intent/tweet?text=${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/thumbnail.png?level=${level}&challenges=${challengesCompleted}&experience=${totalExperience}`}>Compartilhar no Twitter <Twitter /></a>
             </button>
         </footer>
       </div>
