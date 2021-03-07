778b0c59c59abfd780dba8c97366d61523808f9a
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-07 08:02:51 -0300

    Fixing bugs

--- src/pages/challenges.tsx
+++ src/pages/challenges.tsx
@@ -1,106 +1,102 @@
-// import { CompletedChallenges } from "../components/CompletedChallenges";
-// import { Countdown } from "../components/Countdown";
-// import { ExperienceBar } from "../components/ExperienceBar";
-// import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
 
-// import Head from 'next/head'
+import Head from 'next/head'
 
-// import { ChallengeBox } from "../components/ChallengeBox";
-// import { CountdownProvider } from "../contexts/CountdownContext";
-// import { GetServerSideProps } from "next";
-// import { ChallengesProvider } from "../contexts/ChallengesContext";
-// import { getSession } from "next-auth/client";
-// import {useRouter} from 'next/router'
-// import Layout from "../components/Layout";
-// import { Container } from "../styles/pages/HomeStyles";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
+import { GetServerSideProps } from "next";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
+import { getSession } from "next-auth/client";
+import {useRouter} from 'next/router'
+import Layout from "../components/Layout";
+import { Container } from "../styles/pages/HomeStyles";
 
-// interface Badge {
-//   badge_id:string;
-//   description:string;
-//   image:string;
-// }
+interface Badge {
+  badge_id:string;
+  description:string;
+  image:string;
+}
 
-// interface Session{
-//   userId:string;
-// }
+interface Session{
+  userId:string;
+}
 
-// interface User{
-//   id: string;
-//   name:string;
-//   image:string;
-//   challengesCompleted:number;
-//   level:number;
-//   currentExperience:number;
-//   totalExperience:number;
-//   achievements:Badge[],
-//   badges: object;
-// }
+interface User{
+  id: string;
+  name:string;
+  image:string;
+  challengesCompleted:number;
+  level:number;
+  currentExperience:number;
+  totalExperience:number;
+  achievements:Badge[],
+  badges: object;
+}
 
-// interface HomeProps {
-//   level: number;
-//   currentExperience: number;
-//   challengesCompleted: number;
-//   sessions: Session;
-//   user: User;
-// }
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  sessions: Session;
+  user: User;
+}
 
-// export default function Challenges(props:HomeProps) {
-//   const router = useRouter();
+export default function Challenges(props:HomeProps) {
+  const router = useRouter();
 
-//   if (typeof window !== 'undefined' && !props.sessions){
-//     router.push("/");
-//   }
+  if (typeof window !== 'undefined' && !props.sessions){
+    router.push("/");
+  }
 
-//   if (props.sessions) return (
-//     <ChallengesProvider id={props.sessions.userId} user_badges={props.user.achievements} level={props.user.level} currentExperience={props.user.currentExperience} challengesCompleted={props.user.challengesCompleted} totalExperience={props.user.totalExperience}>
-//     <Layout>
-//       <Container>
-//         <Head>
-//           <title>Desafios | move.it</title>
-//         </Head>
+  if (props.sessions) return (
+    <ChallengesProvider id={props.sessions.userId} user_badges={props.user.achievements} level={props.user.level} currentExperience={props.user.currentExperience} challengesCompleted={props.user.challengesCompleted} totalExperience={props.user.totalExperience}>
+    <Layout>
+      <Container>
+        <Head>
+          <title>Desafios | move.it</title>
+        </Head>
 
-//         <ExperienceBar />
+        <ExperienceBar />
         
-//         <CountdownProvider>
-//           <section>
-//             <div>
-//               <Profile sessions={props.sessions}/>
-//               <CompletedChallenges />
-//               <Countdown />
-//             </div>
-//             <div>
-//               <ChallengeBox />
-//             </div>
-//           </section>
-//         </CountdownProvider>
-//       </Container>
-//     </Layout>
-//     </ChallengesProvider>
-//   )
-//   else return null
-// }
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile sessions={props.sessions}/>
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </Container>
+    </Layout>
+    </ChallengesProvider>
+  )
+  else return null
+}
 
-// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
-//   var obj;
-//   const session = await getSession(ctx)
-//   if(session){
-//     await fetch(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/challenges?id=${session.userId}`)
-//     .then(res => res.json())
-//     .then(data => obj = data)
-//   }
-//   if(!session){
-//     var obj = null;
-//   }
+  var obj;
+  const session = await getSession(ctx)
+  if(session){
+    await fetch(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/challenges?id=${session.userId}`)
+    .then(res => res.json())
+    .then(data => obj = data)
+  }
+  if(!session){
+    var obj = null;
+  }
   
-//   return {
-//     props: {
-//       sessions: session,
-//       user: obj
-//     }
-//   }
-// }
-
-export default function Challenges() {
-  return <div>challenges</div>
+  return {
+    props: {
+      sessions: session,
+      user: obj
+    }
+  }
 }
--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,116 +1,112 @@
-// import { GetServerSideProps } from "next";
-// import { useSession } from "next-auth/client";
-// import Head from "next/head";
-// import { useRouter } from "next/router";
-// import Layout from "../components/Layout";
-// import { Container } from "../styles/pages/LeaderboardStyles";
+import { GetServerSideProps } from "next";
+import { useSession } from "next-auth/client";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
+import { Container } from "../styles/pages/LeaderboardStyles";
 
-// export default function Leaderboard({users}) {
-//   const mobile = window.matchMedia("(max-width: 560px)").matches;
+export default function Leaderboard({users}) {
+  const mobile = window.matchMedia("(max-width: 560px)").matches;
   
-//   const [ session, loading ] = useSession()
+  const [ session, loading ] = useSession()
 
-//   const router = useRouter();
+  const router = useRouter();
 
-//   if (typeof window !== 'undefined' && !loading && !session){
-//     router.push("/");
-//   }
+  if (typeof window !== 'undefined' && !loading && !session){
+    router.push("/");
+  }
 
-//   const userWithChallenges = users.filter(user => {
-//     if(user.challengesCompleted){
-//       return user
-//     }
-//   })
-//   const formatedUsers = userWithChallenges.sort(function(a,b) {
-//     if (a.challengesCompleted && b.challengesCompleted) {
-//       if(a.challengesCompleted < b.challengesCompleted) return 1;
-//       if (a.challengesCompleted > b.challengesCompleted) return -1;
-//     } else return 0;
-//     return 0;
-//   })
+  const userWithChallenges = users.filter(user => {
+    if(user.challengesCompleted){
+      return user
+    }
+  })
+  const formatedUsers = userWithChallenges.sort(function(a,b) {
+    if (a.challengesCompleted && b.challengesCompleted) {
+      if(a.challengesCompleted < b.challengesCompleted) return 1;
+      if (a.challengesCompleted > b.challengesCompleted) return -1;
+    } else return 0;
+    return 0;
+  })
 
-//   return (
-//     <Layout>
-//       <Head>
-//         <title>Leaderboard | move.it</title>
-//       </Head>
-//       <Container>
-//         <h2>Leaderboard</h2>
-//         <section>
-//           <div className={`titulos ${mobile ? 'mobile' : ''}`}>
-//             <span>Posição</span>
-//             <span>Usuário</span>
-//             {mobile && <div><span>Desafios e Experiência</span></div>}
-//             {!mobile && (
-//               <>
-//                 <span>Desafios</span>
-//                 <span>Experiência</span>
-//               </>
-//             )}
-//           </div>
-//           <section className="grid">
-//             <ul>
-//               {formatedUsers.map((user, index) => (
-//                 <li key={user._id}>
-//                 <span>{index + 1}</span>
+  return (
+    <Layout>
+      <Head>
+        <title>Leaderboard | move.it</title>
+      </Head>
+      <Container>
+        <h2>Leaderboard</h2>
+        <section>
+          <div className={`titulos ${mobile ? 'mobile' : ''}`}>
+            <span>Posição</span>
+            <span>Usuário</span>
+            {mobile && <div><span>Desafios e Experiência</span></div>}
+            {!mobile && (
+              <>
+                <span>Desafios</span>
+                <span>Experiência</span>
+              </>
+            )}
+          </div>
+          <section className="grid">
+            <ul>
+              {formatedUsers.map((user, index) => (
+                <li key={user._id}>
+                <span>{index + 1}</span>
 
-//                 <div>
-//                   <div className="profile">
-//                     <img src={user.image} alt="teste"/>
-//                     <div>
-//                       <strong>{user.name}</strong>
-//                       <p>
-//                         <img src="icons/level.svg" alt="Level"/>
-//                         Level {user.level || 1}
-//                       </p>
-//                     </div>
-//                   </div>
-//                 </div>
+                <div>
+                  <div className="profile">
+                    <img src={user.image} alt="teste"/>
+                    <div>
+                      <strong>{user.name}</strong>
+                      <p>
+                        <img src="icons/level.svg" alt="Level"/>
+                        Level {user.level || 1}
+                      </p>
+                    </div>
+                  </div>
+                </div>
 
-//                 {!mobile && (
-//                   <>
-//                   <div className="info">
-//                   <p><b>{user.challengesCompleted || 0}</b> completados</p>
-//                 </div>
+                {!mobile && (
+                  <>
+                  <div className="info">
+                  <p><b>{user.challengesCompleted || 0}</b> completados</p>
+                </div>
 
-//                 <div className="info">
-//                   <p><b>{user.totalExperience || 0}</b> xp</p>
-//                 </div>
-//                 </>
-//                 )}
+                <div className="info">
+                  <p><b>{user.totalExperience || 0}</b> xp</p>
+                </div>
+                </>
+                )}
 
-//                 {mobile && (
-//                   <div className="info double">
-//                   <p><b>{user.challengesCompleted || 0}</b> completados</p>
-//                   <p><b>{user.totalExperience || 0}</b> xp</p>
-//                 </div>
-//                 )}
+                {mobile && (
+                  <div className="info double">
+                  <p><b>{user.challengesCompleted || 0}</b> completados</p>
+                  <p><b>{user.totalExperience || 0}</b> xp</p>
+                </div>
+                )}
 
-//               </li>
-//               ))}
-//             </ul>
-//           </section>
-//         </section>
-//       </Container>
-//     </Layout>
-//   )
-// }
+              </li>
+              ))}
+            </ul>
+          </section>
+        </section>
+      </Container>
+    </Layout>
+  )
+}
 
-// export const getStaticProps: GetServerSideProps = async () => {
+export const getStaticProps: GetServerSideProps = async () => {
 
-//   var obj;
-//   await fetch(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/challenges?all=1`)
-//   .then(res => res.json())
-//   .then(data => obj = data)
+  var obj;
+  await fetch(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/challenges?all=1`)
+  .then(res => res.json())
+  .then(data => obj = data)
   
-//   return {
-//     props: {
-//       users: obj
-//     },
-//     revalidate: 60,
-//   }
-// }
-
-export default function Challenges() {
-  return <div>challenges</div>
+  return {
+    props: {
+      users: obj
+    },
+    revalidate: 60,
+  }
 }
