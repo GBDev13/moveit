46772f7f3df82b8b8466fd5fb14eed186a1064cc
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-07 21:43:24 -0300

    Changed countdown to 6 seconds (Only for tests)

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,6 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider ({children}: CountdownProviderProps) {
   const {startNewChallenge} = useContext(ChallengesContext)
 
-  // const [time, setTime] = useState(1 * 60);
   const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
@@ -37,7 +36,6 @@ export function CountdownProvider ({children}: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    // setTime(1 * 60);
     setTime(0.1 * 60);
     setHasFinished(false);
   }
